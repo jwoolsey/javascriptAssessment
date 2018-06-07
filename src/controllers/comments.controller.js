d03945e42f80ad3286a22b55e3c317ddee19cf5d
Jennifer Woolsey <jennifer.a.woolsey@gmail.com>
2018-06-06 23:01:46 -0500

    Build out CommentsController

--- src/controllers/comments.controller.js
+++ src/controllers/comments.controller.js
@@ -5,9 +5,26 @@ class CommentsController {
 
   init() {
     // kick off controller from here
+    this.addCommentFormListener();
   }
 
   addCommentFormListener() {
     // create comment form listener code here
+    //Trigger function on form submit
+    var comment = this.comments;
+    for (var i=0; i <= comment.length; i++) {
+      comment[i].addEventListener('submit', newComment, false);
+
+      function newComment (comment) {
+        event.preventDefault();
+        //Grab imageId + comment and create new variable
+        var imageId = parseInt($(this).parents('h2').next('ul').data('id'));
+        comment = $(this).parents('ul').find('.commentContent').value;
+        var commentObj = new Comment(imageId, comment);
+
+        return commentObj;
+      }
+      this.render(commentObj);
+    }
   }
 }
